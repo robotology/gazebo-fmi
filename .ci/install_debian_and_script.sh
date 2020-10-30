663aa3ce41c7e115b7107558e30be284b02011a4
Silvio Traversaro <silvio.traversaro@iit.it>
2020-10-30 14:21:49 +0100

    Use OpenModelica release in Travis instead of nightly

--- .ci/install_debian_and_script.sh
+++ .ci/install_debian_and_script.sh
@@ -12,7 +12,7 @@ sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_rel
 wget http://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
 
 # OpenModelica
-for deb in deb deb-src; do echo "$deb http://build.openmodelica.org/apt `lsb_release -cs` nightly"; done | tee /etc/apt/sources.list.d/openmodelica.list
+for deb in deb deb-src; do echo "$deb http://build.openmodelica.org/apt `lsb_release -cs` release"; done | tee /etc/apt/sources.list.d/openmodelica.list
 wget -q http://build.openmodelica.org/apt/openmodelica.asc -O- | apt-key add -
 
 # Update packages
